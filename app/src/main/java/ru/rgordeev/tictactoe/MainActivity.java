52b8302774454eb45c19931e93be176ff9d2d06b
Roman Gordeev <roma.gordeev@gmail.com>
2021-10-26 17:08:34 +0300

    initial

---
+++ app/src/main/java/ru/rgordeev/tictactoe/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.rgordeev.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
